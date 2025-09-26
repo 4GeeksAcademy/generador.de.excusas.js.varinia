a88c9acfd3144cdae7e707e96d13572d3f63a669
Varinia <v.mesias.r@gmail.com>
2025-09-26 02:20:10 +0000

    arreglo funcion

--- src/app.js
+++ src/app.js
@@ -5,17 +5,20 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
   //write your code here
 
+window.onload = function (){
 
-let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
-let action = ['ate', 'peed', 'crushed', 'broke'];
-let what = ['my homework', 'my phone', 'the car'];
-let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+	let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+	let action = ['eat', 'peed', 'crushed', 'broke'];
+	let what = ['my homework', 'my phone', 'the car'];
+	let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  let randomWho = who[Math.floor(Math.random() * who.length)];
+  let randomAction = action[Math.floor(Math.random() * action.length)];
+  let randomWhat = what[Math.floor(Math.random() * what.length)];
+  let randomWhen = when[Math.floor(Math.random() * when.length)];
+  document.getElementById('excuse').innerText=`${randomWho}, ${randomAction}, ${randomWhat}, ${randomWhen}`;
+  
+};
 
   
-
-  let aleatorio = Math.floor(Math.random() * excuse.length)
-  console.log(excuse[aleatorio]);
-};
